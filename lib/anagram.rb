293d989a42605a076e2a1fa42ce65032b5d44868
Thomas Wesley Matlock <thomasmatlock1994@gmail.com>
2018-03-21 17:05:20 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+  
+  def initialize(word)
+    @word = word
+  end 
+  
+  def match(find_it)
+     find_it.find_all do |word| 
+      if word.split("").sort == self.word.split("").sort 
+        word 
+      end
+    end 
+  end
+ 
+  
+end
