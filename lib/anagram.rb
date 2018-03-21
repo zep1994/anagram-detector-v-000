355d45298bda6905379e4be649be175ac14e2710
Thomas Wesley Matlock <thomasmatlock1994@gmail.com>
2018-03-21 17:04:38 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,7 +7,11 @@ class Anagram
   end 
   
   def match(find_it)
-    
+     find_anagram_in_this_string.find_all do |word| 
+      if word.split("").sort == self.word_to_analyze.split("").sort 
+        word 
+      end
+    end 
   end
  
   
